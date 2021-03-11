c975e94a75516a429d126506f78b549be83fe498
jmcadam <mcadam88@gmail.com>
2021-03-11 12:40:39 +0000

    add IAC create scn

--- src/gatling/simulations/uk/gov/hmcts/wa/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/wa/scenarios/utils/Environment.scala
@@ -13,4 +13,13 @@ object Environment {
   val maxWaitForNextIteration = 120 //240
   val HttpProtocol = http
 
+  val idamURL = "https://idam-web-public.perftest.platform.hmcts.net"
+  val idamAPI = "https://idam-api.perftest.platform.hmcts.net"
+  val ccdEnvurl = "https://www-ccd.perftest.platform.hmcts.net"
+  val ccdDataStoreUrl = "ccd-data-store-api-perftest.service.core-compute-perftest.internal"
+  val baseURL = "https://gateway-ccd.perftest.platform.hmcts.net"
+  val s2sUrl = "http://rpe-service-auth-provider-perftest.service.core-compute-perftest.internal"
+  val xuiMCUrl = "https://manage-case.perftest.platform.hmcts.net/oauth2/callback"
+  val xuiBaseURL = "https://manage-case.perftest.platform.hmcts.net"
+  val dmStore = "http://dm-store-perftest.service.core-compute-perftest.internal"
 }
