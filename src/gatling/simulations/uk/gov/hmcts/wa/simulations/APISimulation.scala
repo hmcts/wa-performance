116b25d1f611901cf4806acbf58572e5b8d3339c
jmcadam <mcadam88@gmail.com>
2021-12-21 10:54:36 +0000

    updates ready for R2

--- src/gatling/simulations/uk/gov/hmcts/wa/simulations/APISimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/wa/simulations/APISimulation.scala
@@ -11,31 +11,32 @@ class APISimulation extends Simulation  {
 
   val BaseURL = Environment.baseURL
 
+  val feedCompleteTaskListFeeder = csv("WA_TasksToComplete.csv")
+  val feedWATribunalUserData = csv("WA_TribunalUsers.csv").circular
+  val feedWASeniorUserData = csv("WA_SeniorTribunalUsers.csv").circular
+  val taskListFeeder = csv("WA_TaskList.csv").circular
+  val feedIACUserData = csv("IACUserData.csv").circular
+
   val httpProtocol = Environment.HttpProtocol
     .baseUrl(BaseURL)
     .doNotTrackHeader("1")
 
   val WAGetTask = scenario("Work Allocation API - Get Task")
     .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
+      feed(feedWASeniorUserData)
+      .exec(S2S.s2s("wa_task_management_api"))
+      .exec(IdamLogin.GetIdamToken)
       .repeat(1) { 
-        exec(wataskmanagement.GetTask)
-      }
-    }
-
-  val WASearchCompletable = scenario("Work Allocation API - Search for completable tasks")
-    .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
-      .repeat(1) { 
-        exec(wataskmanagement.PostTaskSearchCompletable)
+        feed(taskListFeeder)
+        .exec(wataskmanagement.GetTask)
       }
     }
 
   val IACCaseCreate = scenario("IAC Case Create via CCD")
     .repeat(1) {
-      exec(ccddatastore.ccdIdamLogin)
+      feed(feedIACUserData)
+      .exec(S2S.s2s("ccd_data"))
+      .exec(IdamLogin.GetIdamToken)
       .repeat(1) {  //10
         exec(ccddatastore.ccdCreateCase)
         .exec(ccddatastore.ccdSubmitAppeal)
@@ -43,58 +44,25 @@ class APISimulation extends Simulation  {
       }
     }
 
-  val WAPostRetrieveTask = scenario("Work Allocation API - POST Retrieve Task")
-    .repeat(1) {
-      exec(ccddatastore.ccdIdamLogin)
-      .repeat(1) { 
-        exec(wataskmanagement.PostTaskRetrieve)
-      }
-    }
-
-  val WAClaimTask = scenario("Work Allocation API - Claim Task")
-    .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
-      .repeat(1) { 
-        exec(wataskmanagement.ClaimTask)
-      }
-    }
-
-  val WAUnclaimTask = scenario("Work Allocation API - Unclaim Task")
-    .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
-      .repeat(1) { 
-        exec(wataskmanagement.UnclaimTask)
-      }
-    }
-
-  val WAClaimUnclaimTask = scenario("Work Allocation API - Claim and then Unclaim a Task")
-    .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
-      .repeat(1) { 
-        exec(wataskmanagement.ClaimTask)
-        .exec(wataskmanagement.UnclaimTask)
-      }
-    }
-
   val WACompleteTask = scenario("Work Allocation API - Complete a Task")
     .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
-      .repeat(6) {
-        exec(wataskmanagement.GetTaskForCompletion)
+      feed(feedWASeniorUserData)
+      .exec(S2S.s2s("wa_task_management_api"))
+      .exec(IdamLogin.GetIdamToken)
+      .repeat(1) { //6
+        feed(feedCompleteTaskListFeeder)
+        .exec(wataskmanagement.GetTaskForCompletion)
         .exec(wataskmanagement.PostAssignTask)
         .exec(wataskmanagement.CompleteTask)
-        .exec(WaitforNextIteration.waitforNextIteration)
+        // .exec(WaitforNextIteration.waitforNextIteration)
       }
     }
 
   val WACancelTask = scenario("Work Allocation API - Cancel a Task")
     .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
+      feed(feedWASeniorUserData)
+      .exec(S2S.s2s("wa_task_management_api"))
+      .exec(IdamLogin.GetIdamToken)
       .repeat(500) { //8
         // exec(wataskmanagement.GetTask)
         exec(wataskmanagement.CancelTask)
@@ -102,10 +70,20 @@ class APISimulation extends Simulation  {
       }
     }
 
+  val WAAssignTask = scenario ("Assign a Task")
+    .repeat(1) {
+      feed(feedWASeniorUserData)
+      .exec(S2S.s2s("wa_task_management_api"))
+      .exec(IdamLogin.GetIdamToken)
+      .repeat(1) { 
+        feed(taskListFeeder)
+        .exec(wataskmanagement.PostAssignTask)
+      }
+    }
   val CamundaGetCase = scenario("Camunda DB - Get Case details")
     .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .repeat(5000) {
+      exec(S2S.s2s("wa_task_management_api"))
+      .repeat(1) { //5000
         exec(wataskmanagement.CamundaGetCase)
       }
     }
@@ -135,28 +113,32 @@ class APISimulation extends Simulation  {
     //   .pause(10)
     //   }
     .repeat(1) {//Update for required number of tasks
-      exec(wataskmanagement.WATaskS2SLogin)
-      .repeat(500) {
+      exec(S2S.s2s("wa_case_event_handler"))
+      .repeat(1) {
         exec(wataskmanagement.CreateTask)
       }
     }
 
   val GetAllTasks = scenario("WA - Get All Tasks")
     .repeat(1) {
-      exec(wataskmanagement.WAS2SLogin)
-      .exec(wataskmanagement.WASeniorIdamLogin)
-      .repeat(10) { 
+      feed(feedWASeniorUserData)
+      .exec(S2S.s2s("wa_case_event_handler"))
+      .exec(IdamLogin.GetIdamToken)
+      .repeat(1) { 
         exec(wataskmanagement.GetAllTasks)
       }
     }
 
   setUp(
     // TaskManagerTests.inject(rampUsers(1) during (1 minutes))
-    GetAllTasks.inject(rampUsers(1) during (1 minutes))
+    // WACompleteTask.inject(rampUsers(1) during (1 minutes))
+    WAAssignTask.inject(rampUsers(1) during (1 minutes))
+    // GetAllTasks.inject(rampUsers(1) during (1 minutes))
+    // CamundaGetCase.inject(rampUsers(1) during (1 minutes))
     // WAGetTask.inject(rampUsers(1) during (1 minutes))
 
     //Scenarios required for perf test
-    // IACCaseCreate.inject(rampUsers(4) during (5 minutes)),
+    // IACCaseCreate.inject(rampUsers(1) during (5 minutes)),
     // WACompleteTask.inject(rampUsers(6) during (5 minutes)),
     // WACancelTask.inject(rampUsers(4) during (5 minutes)),
     // WAGetTask.inject(rampUsers(80) during (7 minutes))
