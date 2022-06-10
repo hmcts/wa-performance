09c06eba99035fd558d27a4f9499f2ff17fd74d4
jmcadam <mcadam88@gmail.com>
2022-06-10 13:36:24 +0100

    data update

--- src/gatling/simulations/uk/gov/hmcts/wa/simulations/UISimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/wa/simulations/UISimulation.scala
@@ -62,7 +62,7 @@ class UISimulation extends Simulation  {
       exec(xuiwa.manageCasesHomePage)
       .feed(feedTribunalUserData)
       .exec(xuiwa.manageCasesLogin)
-      .repeat(40) { //10
+      .repeat(10) { //10
         exec(xuiAllWork.allWorkTasks)
         .feed(feedCaseList)
         .exec(xuiAllWork.allWorkViewTask)
@@ -115,11 +115,11 @@ class UISimulation extends Simulation  {
   
   setUp(
     R2AssignAndCompleteTasks.inject(rampUsers(60) during (10 minutes)), //60 during 10
-    // R2CancelTask.inject(rampUsers(5) during (20 minutes)),
-    // CreateTaskFromCCD.inject(rampUsers(15) during (10 minutes)),
-    // R2JudicialUserJourney.inject(rampUsers(36) during (2 minutes))
+    R2CancelTask.inject(rampUsers(5) during (20 minutes)),
+    CreateTaskFromCCD.inject(rampUsers(15) during (10 minutes)),
+    R2JudicialUserJourney.inject(rampUsers(36) during (2 minutes))
     )
-    // .maxDuration(60 minutes)
+    .maxDuration(60 minutes)
     .protocols(httpProtocol)
 
   // setUp(
