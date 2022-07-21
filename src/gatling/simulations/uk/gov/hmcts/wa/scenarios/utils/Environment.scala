a17504157e5cceafe1ea1a90b07780adc6ca5b40
jmcadam <mcadam88@gmail.com>
2022-07-21 15:52:27 +0100

    update data and switch to open load model

--- src/gatling/simulations/uk/gov/hmcts/wa/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/wa/scenarios/utils/Environment.scala
@@ -13,19 +13,19 @@ object Environment {
   val maxWaitForNextIteration = 240 //240
   val HttpProtocol = http
 
-  val idamURL = "https://idam-web-public.perftest.platform.hmcts.net"
-  val idamAPI = "https://idam-api.perftest.platform.hmcts.net"
-  val ccdEnvurl = "https://www-ccd.perftest.platform.hmcts.net"
-  val ccdDataStoreUrl = "ccd-data-store-api-perftest.service.core-compute-perftest.internal"
-  val baseURL = "https://gateway-ccd.perftest.platform.hmcts.net"
-  val s2sUrl = "http://rpe-service-auth-provider-perftest.service.core-compute-perftest.internal"
-  val xuiMCUrl = "https://manage-case.perftest.platform.hmcts.net/oauth2/callback"
-  val xuiBaseURL = "https://manage-case.perftest.platform.hmcts.net"
-  val dmStore = "http://dm-store-perftest.service.core-compute-perftest.internal"
-  val waTMURL = "http://wa-task-management-api-perftest.service.core-compute-perftest.internal"
-  val ccdRedirectUri = "https://ccd-data-store-api-perftest.service.core-compute-perftest.internal/oauth2redirect"
-  val camundaURL = "http://camunda-api-perftest.service.core-compute-perftest.internal"
-  val waWorkflowApiURL = "http://wa-workflow-api-perftest.service.core-compute-perftest.internal"
+  val idamURL = "https://idam-web-public.${env}.platform.hmcts.net"
+  val idamAPI = "https://idam-api.${env}.platform.hmcts.net"
+  val ccdEnvurl = "https://www-ccd.${env}.platform.hmcts.net"
+  val ccdDataStoreUrl = "ccd-data-store-api-${env}.service.core-compute-${env}.internal"
+  val baseURL = "https://gateway-ccd.${env}.platform.hmcts.net"
+  val s2sUrl = "http://rpe-service-auth-provider-${env}.service.core-compute-${env}.internal"
+  val xuiMCUrl = "https://manage-case.${env}.platform.hmcts.net/oauth2/callback"
+  val xuiBaseURL = "https://manage-case.${env}.platform.hmcts.net"
+  val dmStore = "http://dm-store-${env}.service.core-compute-${env}.internal"
+  val waTMURL = "http://wa-task-management-api-${env}.service.core-compute-${env}.internal"
+  val ccdRedirectUri = "https://ccd-data-store-api-${env}.service.core-compute-${env}.internal/oauth2redirect"
+  val camundaURL = "http://camunda-api-${env}.service.core-compute-${env}.internal"
+  val waWorkflowApiURL = "http://wa-workflow-api-${env}.service.core-compute-${env}.internal"
 
   val ClearSessionVariables =
     exec(flushHttpCache)
