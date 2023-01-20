6d7758937db0d3160c407c4c86ba1a88e89eca58
jmcadam <mcadam88@gmail.com>
2023-01-20 12:49:34 +0000

    data prep

--- src/gatling/simulations/uk/gov/hmcts/wa/simulations/UISimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/wa/simulations/UISimulation.scala
@@ -48,8 +48,8 @@ class UISimulation extends Simulation  {
 	/* PERFORMANCE TEST CONFIGURATION */
 	val assignAndCompleteTargetPerHour: Double = 700 //700
 	val cancelTaskTargetPerHour: Double = 40 //40
-	val iacCreateTargetPerHour: Double = 1500 //1500
-  val civilCompleteTargetPerHour: Double = 200 //200
+	val iacCreateTargetPerHour: Double = 4500 //1500
+  val civilCompleteTargetPerHour: Double = 2000 //200
   val civilJudicialCompleteTargetPerHour: Double = 150 //150
 	val judicialTargetPerHour: Double = 360 //360
   // val prlTargetPerHour: Double = 150 //150
@@ -279,7 +279,7 @@ class UISimulation extends Simulation  {
     // R2JudicialUserJourney.inject(simulationProfile(testType, judicialTargetPerHour, numberOfPipelineUsers)).pauses(pauseOption),                         //
     // CivilAssignAndCompleteTask.inject(simulationProfile(testType, civilJudicialCompleteTargetPerHour, numberOfPipelineUsers)).pauses(pauseOption),       //
     CreateCivilDJTaskFromCCD.inject(simulationProfile(testType, civilCompleteTargetPerHour, numberOfPipelineUsers)).pauses(pauseOption),                 //
-    // CreateIACTaskFromCCD.inject(simulationProfile(testType, iacCreateTargetPerHour, numberOfPipelineUsers)).pauses(pauseOption),                         //
+    CreateIACTaskFromCCD.inject(simulationProfile(testType, iacCreateTargetPerHour, numberOfPipelineUsers)).pauses(pauseOption),                         //
     // CreatePRLTaskFromCCD.inject(simulationProfile(testType, prlTargetPerHour, numberOfPipelineUsers)).pauses(pauseOption),                               //
 
     // getTaskFromCamunda.inject(rampUsers(1) during (1 minute))
