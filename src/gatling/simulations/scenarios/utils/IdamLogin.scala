babd4e78eb4546c3bb827717baf077be3c42350c
jmcadam <mcadam88@gmail.com>
2023-05-22 15:44:46 +0100

    update clientsecret config

---
+++ src/gatling/simulations/scenarios/utils/IdamLogin.scala
@@ -0,0 +1,22 @@
+package utils
+
+import com.typesafe.config.{Config, ConfigFactory}
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import utils._
+
+object IdamLogin {
+
+  val config: Config = ConfigFactory.load()
+  val ccdScope = "openid profile authorities acr roles openid profile roles"
+  val ccdGatewayClientSecret = config.getString("auth.clientSecret")
+
+  val GetIdamToken =
+
+    exec(http("GetIdamToken")
+      .post(Environment.idamAPI + "/o/token?client_id=ccd_gateway&client_secret=" + ccdGatewayClientSecret + "&grant_type=password&scope=" + ccdScope + "&username=#{email}&password=#{password}")
+      .header("Content-Type", "application/x-www-form-urlencoded")
+      .header("Content-Length", "0")
+      .check(status.is(200))
+      .check(jsonPath("$.access_token").saveAs("access_token")))
+}
