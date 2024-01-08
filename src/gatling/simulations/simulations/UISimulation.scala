d832bcee0d957f42638d025061e52edeb1cb4c63
jmcadam <mcadam88@gmail.com>
2024-01-08 10:46:23 +0000

    update civil UI journey & prep for data-gen

--- src/gatling/simulations/simulations/UISimulation.scala
+++ src/gatling/simulations/simulations/UISimulation.scala
@@ -226,18 +226,19 @@ class UISimulation extends Simulation  {
       .repeat(1) {
         exec(S2S.s2s("xui_webapp"))
         .exec(IdamLogin.GetIdamTokenPayments)
-        // .exec(ccddatastore.civilCreateCase)
+        .exec(ccddatastore.civilCreateCase)
         .pause(60)
-        .feed(feedCivilCaseList) // use this for manually putting a case ID in when running this is debug mode, and you 
+        // .feed(feedCivilCaseList) // use this for manually putting a case ID in when running this is debug mode, and you 
         // have to run each case event in turn
+        .exec(S2S.s2s("civil_service"))
         .exec(ccddatastore.civilAddPayment)
-        // .pause(60)
-        // .exec(ccddatastore.civilNotifyClaim)
-        // .pause(60)
-        // .exec(ccddatastore.civilNotifyClaimDetails)
-        // .pause(60)
-        // .exec(ccddatastore.civilUpdateDate)
-        // .exec(ccddatastore.civilRequestDefaultJudgement)
+        .pause(60)
+        .exec(ccddatastore.civilNotifyClaim)
+        .pause(60)
+        .exec(ccddatastore.civilNotifyClaimDetails)
+        .pause(60)
+        .exec(ccddatastore.civilUpdateDate)
+        .exec(ccddatastore.civilRequestDefaultJudgement)
       }
     }
 
