750e40800829f1177dfaa3ca467467587d2bb36c
jmcadam <mcadam88@gmail.com>
2024-10-16 10:47:00 +0100

    add global search grouping

--- src/gatling/simulations/scenarios/ui/xuiPrl.scala
+++ src/gatling/simulations/scenarios/ui/xuiPrl.scala
@@ -11,38 +11,40 @@ object xuiPrl {
 
   val SearchCase = 
 
-    exec(Common.isAuthenticated)
-    .exec(Common.apiUserDetails)
+    group("XUI_GlobalSearch_Request") {
+      exec(Common.isAuthenticated)
+      .exec(Common.apiUserDetails)
 
-    .exec(http("XUI_GlobalSearch_010_Services")
-			.get("/api/globalSearch/services")
-			.headers(XUIHeaders.xuiMainHeader))
+      .exec(http("XUI_GlobalSearch_010_Services")
+        .get("/api/globalSearch/services")
+        .headers(XUIHeaders.xuiMainHeader))
 
-    .exec(http("XUI_GlobalSearch_010_JurisdictionsRead")
-			.get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json"))
+      .exec(http("XUI_GlobalSearch_010_JurisdictionsRead")
+        .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json"))
 
-    .pause(Environment.constantthinkTime)
+      .pause(Environment.constantthinkTime)
 
-    .exec(http("XUI_GlobalSearch_020_Request")
-			.post("/api/globalsearch/results")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json")
-      .header("x-xsrf-token", "#{xsrfToken}")
-			.body(ElFileBody("xuiBodies/FPLCaseSearch.json")))
+      .exec(http("XUI_GlobalSearch_020_Request")
+        .post("/api/globalsearch/results")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json")
+        .header("x-xsrf-token", "#{xsrfToken}")
+        .body(ElFileBody("xuiBodies/FPLCaseSearch.json")))
 
-    .exec(Common.isAuthenticated)
-		
-		.exec(http("XUI_GlobalSearch_020_GetCase")
-			.get("/data/internal/cases/#{caseId}")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json"))
+      .exec(Common.isAuthenticated)
+      
+      .exec(http("XUI_GlobalSearch_020_GetCase")
+        .get("/data/internal/cases/#{caseId}")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json"))
 
-    .exec(Common.apiUserDetails)
+      .exec(Common.apiUserDetails)
+    }
 
     .pause(Environment.constantthinkTime)
 
--- src/gatling/simulations/scenarios/ui/xuiSscs.scala
+++ src/gatling/simulations/scenarios/ui/xuiSscs.scala
@@ -11,49 +11,51 @@ object xuiSscs {
 
   val SearchCase = 
 
-    exec(http("XUI_GlobalSearch_010_IsAuthenticated")
-			.get("/auth/isAuthenticated")
-			.headers(XUIHeaders.xuiMainHeader))
+    group("XUI_GlobalSearch_Request") {
+      exec(http("XUI_GlobalSearch_010_IsAuthenticated")
+        .get("/auth/isAuthenticated")
+        .headers(XUIHeaders.xuiMainHeader))
 
-    .exec(http("XUI_GlobalSearch_010_ApiUserDetails")
-			.get("/api/user/details")
-			.headers(XUIHeaders.xuiMainHeader))
+      .exec(http("XUI_GlobalSearch_010_ApiUserDetails")
+        .get("/api/user/details")
+        .headers(XUIHeaders.xuiMainHeader))
 
-    .exec(http("XUI_GlobalSearch_010_Services")
-			.get("/api/globalSearch/services")
-			.headers(XUIHeaders.xuiMainHeader))
+      .exec(http("XUI_GlobalSearch_010_Services")
+        .get("/api/globalSearch/services")
+        .headers(XUIHeaders.xuiMainHeader))
 
-    .exec(http("XUI_GlobalSearch_010_JurisdictionsRead")
-			.get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json"))
+      .exec(http("XUI_GlobalSearch_010_JurisdictionsRead")
+        .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json"))
 
-    .pause(Environment.constantthinkTime)
+      .pause(Environment.constantthinkTime)
 
-    .exec(http("XUI_GlobalSearch_020_Request")
-			.post("/api/globalsearch/results")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json")
-      .header("x-xsrf-token", "#{xsrfToken}")
-			.body(ElFileBody("xuiBodies/FPLCaseSearch.json")))
+      .exec(http("XUI_GlobalSearch_020_Request")
+        .post("/api/globalsearch/results")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json")
+        .header("x-xsrf-token", "#{xsrfToken}")
+        .body(ElFileBody("xuiBodies/FPLCaseSearch.json")))
 
-    .exec(http("XUI_GlobalSearch_020_IsAuthenticated")
-			.get("/auth/isAuthenticated")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*"))
-		
-		.exec(http("XUI_GlobalSearch_020_GetCase")
-			.get("/data/internal/cases/#{caseId}")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json"))
+      .exec(http("XUI_GlobalSearch_020_IsAuthenticated")
+        .get("/auth/isAuthenticated")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*"))
+      
+      .exec(http("XUI_GlobalSearch_020_GetCase")
+        .get("/data/internal/cases/#{caseId}")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json"))
 
-    .exec(http("XUI_GlobalSearch_020_ApiUserDetails")
-			.get("/api/user/details")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*"))
+      .exec(http("XUI_GlobalSearch_020_ApiUserDetails")
+        .get("/api/user/details")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*"))
+    }
 
     .pause(Environment.constantthinkTime)
 
