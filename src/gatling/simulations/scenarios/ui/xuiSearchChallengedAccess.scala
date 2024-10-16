3e7a3454a97d675122e3f533f2dc31fbf0913974
jmcadam <mcadam88@gmail.com>
2024-10-16 10:47:00 +0100

    add global search grouping

--- src/gatling/simulations/scenarios/ui/xuiSearchChallengedAccess.scala
+++ src/gatling/simulations/scenarios/ui/xuiSearchChallengedAccess.scala
@@ -16,51 +16,53 @@ object xuiSearchChallengedAccess {
 
   val GlobalSearch =
 
-    exec(http("XUI_GlobalSearch_010_IsAuthenticated")
-			.get("/auth/isAuthenticated")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*"))
+    group("XUI_GlobalSearch_Request") {
+      exec(http("XUI_GlobalSearch_010_IsAuthenticated")
+        .get("/auth/isAuthenticated")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*"))
 
-    .exec(http("XUI_GlobalSearch_010_Services")
-			.get("/api/globalSearch/services")
-			.headers(XUIHeaders.xuiMainHeader))
+      .exec(http("XUI_GlobalSearch_010_Services")
+        .get("/api/globalSearch/services")
+        .headers(XUIHeaders.xuiMainHeader))
 
-    .exec(http("XUI_GlobalSearch_010_ApiUserDetails")
-			.get("/api/user/details")
-			.headers(XUIHeaders.xuiMainHeader))
-		
-		// .exec(http("XUI_GlobalSearch_010_JurisdictionsRead")
-		// 	.get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
-		// 	.headers(XUIHeaders.xuiMainHeader)
-    //   .header("accept", "application/json, text/plain, */*")
-    //   .header("content-type", "application/json"))
+      .exec(http("XUI_GlobalSearch_010_ApiUserDetails")
+        .get("/api/user/details")
+        .headers(XUIHeaders.xuiMainHeader))
+      
+      // .exec(http("XUI_GlobalSearch_010_JurisdictionsRead")
+      // 	.get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
+      // 	.headers(XUIHeaders.xuiMainHeader)
+      //   .header("accept", "application/json, text/plain, */*")
+      //   .header("content-type", "application/json"))
 
-    .pause(Environment.constantthinkTime)
+      .pause(Environment.constantthinkTime)
 
-    .exec(http("XUI_GlobalSearch_020_Request")
-			.post("/api/globalsearch/results")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json")
-      .header("x-xsrf-token", "#{xsrfToken}")
-			.body(ElFileBody("xuiBodies/GlobalSearchRequest.json"))
-      .check(jsonPath("$.results[*].processForAccess").optional.saveAs("accessRequired")))
-            		
-		.exec(http("XUI_GlobalSearch_020_IsAuthenticated")
-			.get("/auth/isAuthenticated")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*"))
-		
-		.exec(http("XUI_GlobalSearch_020_GetCase")
-			.get("/data/internal/cases/#{caseId}")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*")
-      .header("content-type", "application/json"))
+      .exec(http("XUI_GlobalSearch_020_Request")
+        .post("/api/globalsearch/results")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json")
+        .header("x-xsrf-token", "#{xsrfToken}")
+        .body(ElFileBody("xuiBodies/GlobalSearchRequest.json"))
+        .check(jsonPath("$.results[*].processForAccess").optional.saveAs("accessRequired")))
+                  
+      .exec(http("XUI_GlobalSearch_020_IsAuthenticated")
+        .get("/auth/isAuthenticated")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*"))
+      
+      .exec(http("XUI_GlobalSearch_020_GetCase")
+        .get("/data/internal/cases/#{caseId}")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*")
+        .header("content-type", "application/json"))
 
-    .exec(http("XUI_GlobalSearch_020_ApiUserDetails")
-			.get("/api/user/details")
-			.headers(XUIHeaders.xuiMainHeader)
-      .header("accept", "application/json, text/plain, */*"))
+      .exec(http("XUI_GlobalSearch_020_ApiUserDetails")
+        .get("/api/user/details")
+        .headers(XUIHeaders.xuiMainHeader)
+        .header("accept", "application/json, text/plain, */*"))
+    }
 
     .pause(Environment.constantthinkTime)
 
