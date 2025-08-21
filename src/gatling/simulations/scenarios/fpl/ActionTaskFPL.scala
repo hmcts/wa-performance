c9bf6d618cff1af943b8bad8d71c4397c96f50c8
jmcadam <mcadam88@gmail.com>
2025-08-21 17:33:54 +0100

    amend session variable name

--- src/gatling/simulations/scenarios/fpl/ActionTaskFPL.scala
+++ src/gatling/simulations/scenarios/fpl/ActionTaskFPL.scala
@@ -24,7 +24,7 @@ object ActionTaskFPL {
     .exec(_.set("taskName", "reviewMessageHearingCentreAdmin"))
     .exec(ViewCase.execute)
     .feed(randomFeeder)
-    .doIfOrElse(session => if (debugMode == "off") session("cancel-percentage").as[Int] < completePercentage else true) {
+    .doIfOrElse(session => if (debugMode == "off") session("complete-percentage").as[Int] < completePercentage else true) {
       exec(AssignTask.execute)
       .exec(ReplyToMessage.execute)
     }
