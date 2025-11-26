131a044211017dfe02152fbfa919a591da8e6946
jmcadam <mcadam88@gmail.com>
2025-11-26 15:52:35 +0000

    add create payloads & execute steps

--- src/gatling/simulations/scenarios/bails/ActionTaskBails.scala
+++ src/gatling/simulations/scenarios/bails/ActionTaskBails.scala
@@ -1,5 +1,26 @@
 package scenarios.bails
 
+import io.gatling.core.Predef._
+import scenarios.bails.actions._
+import scenarios.common.wa._
+import scenarios.common.xui._
+import xui.XuiHelper
+
+import scala.util.Random
+
 object ActionTaskBails {
 
+  val completePercentage = 90 //Percentage of Complete Tasks //90
+  val randomFeeder = Iterator.continually(Map("complete-percentage" -> Random.nextInt(100)))
+  val feedTribunalUserData = csv("IAStaffUserData.csv").circular
+  val debugMode = System.getProperty("debug", "off")
+
+  val execute =
+
+    feed(feedTribunalUserData)
+    .exec(XuiHelper.Homepage)
+    .exec(XuiHelper.Login("#{user}", "#{password}"))
+    .exec(SearchCase.execute)
+    .exec(_.set("taskName", "processBailApplication"))
+    .exec(XuiHelper.Logout)
 }
--- src/gatling/simulations/scenarios/bails/CreateTaskBails.scala
+++ src/gatling/simulations/scenarios/bails/CreateTaskBails.scala
@@ -1,5 +1,29 @@
 package scenarios.bails
 
+import ccd._
+import io.gatling.core.Predef._
+import utilities.DateUtils
+
+import scala.util.Random
+
 object CreateTaskBails {
+
+  val feedIACUserData = csv("IACUserData.csv").circular
+
+  val rnd = new Random()
+
+  def randomString(length: Int) = {
+    rnd.alphanumeric.filter(_.isLetter).take(length).mkString
+  }
+
+  val execute =
+
+    exec(_.setAll("randomString" -> randomString(5),
+      "dob" -> DateUtils.getDatePastRandom("yyyy-MM-dd", minYears = 20, maxYears = 50)))
+
+      .feed(feedIACUserData)
+
+      .exec(CcdHelper.createCase("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "startApplication", "bailsBodies/BailsCreateCase.json"))
+      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "submitApplication", "bailsBodies/BailsSubmitAppeal.json"))
 
 }
