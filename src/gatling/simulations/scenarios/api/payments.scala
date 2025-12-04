44cf3d69a37b14bead38b9ab4f4f0b4a83d3aa3c
jmcadam <mcadam88@gmail.com>
2025-12-04 16:42:13 +0000

    rework payments auth flow

--- src/gatling/simulations/scenarios/api/payments.scala
+++ src/gatling/simulations/scenarios/api/payments.scala
@@ -4,7 +4,6 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import utilities.AzureKeyVault
 import utils.Environment
-import ccd._
 
 object payments {
 
@@ -15,32 +14,48 @@ object payments {
 
   val AddCivilPayment =
 
-    exec(CcdHelper.authenticate("#{email}", "#{password}", microservice, clientId))
-
-    .exec(http("CCD_AuthLease")
+    exec(http("CCD_AuthLease")
       .post(Environment.rpeUrl + "/testing-support/lease")
-      .body(StringBody(s"""{"microservice":"$civilmicroservice"}""")).asJson
-      .check(regex("(.+)").saveAs("civil_serviceBearerToken"))
+      .body(StringBody(s"""{"microservice":"$microservice"}""")).asJson
+      .check(regex("(.+)").saveAs("xui_webappAuthToken"))
+    )
+
+    .exec(http("CCD_GetBearerToken")
+      .post(Environment.idamAPI + "/o/token")
+      .formParam("grant_type", "password")
+      .formParam("username", "#{email}")
+      .formParam("password", "#{password}")
+      .formParam("client_id", clientId)
+      .formParam("client_secret", clientSecret)
+      .formParam("scope", "openid profile roles search-user")
+      .header("Content-Type", "application/x-www-form-urlencoded")
+      .check(jsonPath("$.access_token").saveAs("access_tokenPayments"))
     )
 
     .exec(http("PaymentAPI_GetCasePaymentOrders")
       .get(Environment.paymentsUrl + "/case-payment-orders?case_ids=#{caseId}")
-      .header("Authorization", "Bearer #{bearerToken}")
-      .header("ServiceAuthorization", "#{authToken}")
+      .header("Authorization", "Bearer #{access_tokenPayments}")
+      .header("ServiceAuthorization", "#{xui_webappAuthToken}")
       .header("Content-Type","application/json")
       .header("accept","*/*")
       .check(jsonPath("$.content[0].orderReference").saveAs("caseIdPaymentRef")))
 
     .pause(Environment.constantthinkTime)
 
+    .exec(http("CCD_AuthLease")
+      .post(Environment.rpeUrl + "/testing-support/lease")
+      .body(StringBody(s"""{"microservice":"$civilmicroservice"}""")).asJson
+      .check(regex("(.+)").saveAs("civil_serviceAuthToken"))
+    )
+
     .tryMax(2) {
       exec(http("API_Civil_AddPayment")
         .put(Environment.civilUrl + "/service-request-update-claim-issued")
-        .header("Authorization", "Bearer #{bearerToken}")
-        .header("ServiceAuthorization", "#{civil_serviceBearerToken}")
+        .header("Authorization", "Bearer #{access_tokenPayments}")
+        .header("ServiceAuthorization", "#{civil_serviceAuthToken}")
         .header("Content-type", "application/json")
         .body(ElFileBody("civilBodies/AddPayment.json")))
-      }
+    }
       
     .pause(Environment.constantthinkTime)
 }
