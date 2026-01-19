b6238197b54415082e418e35b1e85e069705f334
jmcadam <mcadam88@gmail.com>
2026-01-19 18:02:43 +0000

    add extra steps for Bails

--- src/gatling/simulations/scenarios/bails/CreateTaskBails.scala
+++ src/gatling/simulations/scenarios/bails/CreateTaskBails.scala
@@ -3,7 +3,6 @@ package scenarios.bails
 import ccd._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
 import utilities.DateUtils
 
 import scala.util.Random
@@ -14,6 +13,7 @@ object CreateTaskBails {
   val feedIACAdminData = csv("UserDataBailsAdmin.csv").circular
   val feedIACHoData = csv("UserDataBailsHo.csv").circular
   val UserFeederBailsJudge = csv("UserDataBailsJudge.csv").circular
+  val CaseWorkerUserFeeder = csv("CWUserData.csv").circular
 
   val rnd = new Random()
 
@@ -33,11 +33,14 @@ object CreateTaskBails {
       .feed(feedIACAdminData)
       .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "confirmDetentionLocation", "bailsBodies/ConfirmDetentionLocationSubmit.json"))
       .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "caseListing", "bailsBodies/BailsListCase.json"))
-      .feed(feedIACAdminData)
+      .feed(CaseWorkerUserFeeder)
       .exec(CcdHelper.uploadDocumentToCdam("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "120KB.pdf", additionalChecks = Seq(
           jsonPath("$.documents[0]._links.self.href").saveAs("DocumentURL"))))
       .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "uploadBailSummary", "bailsBodies/BailsUploadSummary.json"))
       .feed(UserFeederBailsJudge)
       .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "recordTheDecision", "bailsBodies/BailsJudgeRecordDecision.json"))
+      .exec(CcdHelper.uploadDocumentToCdam("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "CaseSummary.pdf", additionalChecks = Seq(
+            jsonPath("$.documents[0]._links.self.href").saveAs("DocumentURL"))))
+      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "uploadSignedDecisionNotice", "bailsBodies/BailsUploadSignedNotice.json"))
 
 }
