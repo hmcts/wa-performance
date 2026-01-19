d4ee4d4fa449cb11de41feed640558efc7b26e1e
jmcadam <mcadam88@gmail.com>
2026-01-19 16:40:11 +0000

    add additional journeys for Bails case migration testing

--- src/gatling/simulations/scenarios/bails/CreateTaskBails.scala
+++ src/gatling/simulations/scenarios/bails/CreateTaskBails.scala
@@ -2,6 +2,8 @@ package scenarios.bails
 
 import ccd._
 import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
 import utilities.DateUtils
 
 import scala.util.Random
@@ -9,6 +11,9 @@ import scala.util.Random
 object CreateTaskBails {
 
   val feedIACUserData = csv("IACUserData.csv").circular
+  val feedIACAdminData = csv("UserDataBailsAdmin.csv").circular
+  val feedIACHoData = csv("UserDataBailsHo.csv").circular
+  val UserFeederBailsJudge = csv("UserDataBailsJudge.csv").circular
 
   val rnd = new Random()
 
@@ -19,11 +24,20 @@ object CreateTaskBails {
   val execute =
 
     exec(_.setAll("randomString" -> randomString(5),
-      "dob" -> DateUtils.getDatePastRandom("yyyy-MM-dd", minYears = 20, maxYears = 50)))
+      "dob" -> DateUtils.getDatePastRandom("yyyy-MM-dd", minYears = 20, maxYears = 50),
+      "currentDate" -> DateUtils.getDateNow("yyyy-MM-dd")))
 
       .feed(feedIACUserData)
-
       .exec(CcdHelper.createCase("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "startApplication", "bailsBodies/BailsCreateCase.json"))
       .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "submitApplication", "bailsBodies/BailsSubmitAppeal.json"))
+      .feed(feedIACAdminData)
+      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "confirmDetentionLocation", "bailsBodies/ConfirmDetentionLocationSubmit.json"))
+      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "caseListing", "bailsBodies/BailsListCase.json"))
+      .feed(feedIACAdminData)
+      .exec(CcdHelper.uploadDocumentToCdam("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "120KB.pdf", additionalChecks = Seq(
+          jsonPath("$.documents[0]._links.self.href").saveAs("DocumentURL"))))
+      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "uploadBailSummary", "bailsBodies/BailsUploadSummary.json"))
+      .feed(UserFeederBailsJudge)
+      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Bail, "#{caseId}", "recordTheDecision", "bailsBodies/BailsJudgeRecordDecision.json"))
 
 }
